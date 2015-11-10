495f03f40388902dd0902a9728fe9b48d34cb9c9
Tomas Perez <tom@0x101.com>
2015-11-10 21:47:16 +0100

    Restructuring the library to support the new folders organization.

--- test/test.api-client.js
+++ test/test.api-client.js
@@ -3,7 +3,7 @@ var expect = require('expect.js');
 describe('APIClient.Client', function(){
 
   it('Initialization and base object', function(done) {
-    var ApiClient = require('../lib/api-client');
+    var ApiClient = require('../lib/node-api-client');
 
     var client = new ApiClient.Client();
     client.callMethod('test', {}, function(result) {
@@ -18,7 +18,7 @@ describe('APIClient.Client', function(){
 describe('APIClient.Type', function(){
 
   it('API.Type are defined', function() {
-    var ApiClient = require('../lib/api-client');
+    var ApiClient = require('../lib/node-api-client');
     expect(ApiClient.Type).to.be.an('object');
     for (var type in ApiClient.Type) { // jshint ignore:line
       expect(type).to.be.an('string');
