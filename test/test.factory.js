7d33911cdd894546c298c5ce8fc63b5744412e00
Tomas Perez <tom@0x101.com>
2015-11-13 23:01:31 +0100

    Implementing unit test for new use case.

--- test/test.factory.js
+++ test/test.factory.js
@@ -2,9 +2,14 @@ var expect = require('expect.js');
 
 describe('APIClient.Factory', function(){
 
-  it('Factory.get', function() {
+  it('Factory.get with invalid object', function() {
     var ApiClient = require('../lib/node-api-client');
     expect(ApiClient.Factory.get()).to.be.eql(null);
+  });
+
+  it('Factory.get with valid object', function() {
+    var ApiClient = require('../lib/node-api-client');
+    expect(ApiClient.Factory.get('twitter_stream')).to.not.be.eql(null);
   });
 
 });
